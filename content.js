480ce8af98417ae178574c3ec7536934231b0e29
vinesh178 <13551864+vinesh178@users.noreply.github.com>
2023-03-13 20:42:38 +1100

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   
+//     const script = document.querySelector("body > script:nth-child(2)").innerHTML.split("=").slice(1).join('=');
+//   console.log(`script ${script}`);
+//   const regex = /"marketing_price_range":"([^"]+)"/;
+//   const match = regex.exec(script);
+//   const marketingPriceRange = match ? match[1] : null;
+//   console.log(marketingPriceRange); // outputs "750pw_1000pw" for the given script
+
+  
   if (request.type === "myMessage") {
     const addressElement = document.querySelector(
       "#argonaut-wrapper > div.details > div.details__wrapper > div.details__hero > div > div > div.hero-poster__pip > div > div.property-info__header > div.property-info__address-actions > h1"
