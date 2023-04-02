18cf79b7e383e57d8a0dd7e1de3d054b57942c38
Vinesh <vineshk83@gmail.com>
2023-04-02 18:49:58 +1000

    updated lat , long and mpr fetch

--- content.js
+++ content.js
@@ -1,38 +1,43 @@
-const sel = document.querySelector("body > script:nth-child(2)");
+document.addEventListener("DOMContentLoaded", function() {
+ 
+  const sel = document.querySelector("body > script:nth-child(2)");
 
 const property_details = sel.textContent.split("=").slice(1).join("=");
 
 
-const lat = property_details.indexOf("latitude");
-const long = property_details.indexOf("longitude");
-const marketing_price_range = property_details.indexOf("marketing_price_range");
+const latitude = property_details.indexOf("latitude");
+const longitude = property_details.indexOf("longitude");
+const marketing_price_range1 = property_details.indexOf("marketing_price_range");
 
-console.log(property_details.substring(lat, lat + 25).replace(/\\\\\\\"/g, ''));
-console.log(property_details.substring(long, long + 26).replace(/\\\\\\\"/g, ''));
-console.log(
-  property_details.substring(marketing_price_range, marketing_price_range + 57).replace(/\\\\\\\\\\\\\\\"/g,'')
-);
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === "myMessage") {
-    const addressElement = document.querySelector(
-      "#argonaut-wrapper > div.details > div.details__wrapper > div.details__hero > div > div > div.hero-poster__pip > div > div.property-info__header > div.property-info__address-actions > h1"
-    );
+const lat = property_details.substring(latitude, latitude + 25).replace(/[\/\\,]/g, "").split(":")[1].replace(",","")
+const long = property_details.substring(longitude, longitude + 26).replace(/[\/\\,]/g, "").split(":")[1].replace(",","")
 
-    const addressElement2 = document.querySelector(
-      "#argonaut-wrapper > div.details > div.details__wrapper > div.details__main.layout > div.details__content.layout__content > div > div > div.property-info > div.property-info__header > div.property-info__address-actions > h1"
-    );
 
-    const address = addressElement
-      ? addressElement.textContent
-      : addressElement2.textContent;
+const marketing_price_range = property_details.substring(marketing_price_range1, marketing_price_range1 + 57).replace(/[\/\\,]/g, '').split(":")[1].replace(/"/g,"")
 
-    chrome.runtime.sendMessage({ type: "getAddress", address }, (response) => {
-      sendResponse({ school: response.school });
-      return true;
-    });
-  }
-  return true;
+
+const [min, max] = marketing_price_range.split("_");
+
+
+const mpr = `${min} to ${max}`;
+
+chrome.runtime.sendMessage({ type: "getAddress", lat: lat, long: long }, (response) => {
+  const div = document.querySelector("#argonaut-wrapper > div.details > div.details__wrapper > div.details__hero > div > div > div.hero-poster__pip > div > div.property-info__header")
+
+  const div2 = document.querySelector("#argonaut-wrapper > div.details > div.details__wrapper > div.details__main.layout > div.details__content.layout__content > div > div > div.property-info > div.property-info__header > div.property-info__address-actions")
+
+  const finalDiv = div ? div : div2
+
+  finalDiv.id = 'school-name';
+  finalDiv.append(`${response.school} - ${mpr}`)
+
 });
+
+
+
+});
+
+
 
 
